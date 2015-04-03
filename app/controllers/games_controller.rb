33645a263c94bd94a48cae7f863c1394de13a3b8
wengs <wengcheong.sin@gmail.com>
2015-04-03 13:50:35 -0700

    Added FactoryGirl gem, added test for rendering new game page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,13 +1,21 @@
 class GamesController < ApplicationController
+	before_action :authenticate_user!
 	def new
 		@game = Game.new
 	end
 
 	def create
-		
+		@game = current_user.create(game_params)
 	end
 	
 	def show 
 		
 	end
+
+	private
+
+	def game_params
+		params.require(:game).permit(:white_player_id, :black_player_id)
+	end
+
 end
--- test/controllers/games_controller_test.rb
+++ test/controllers/games_controller_test.rb
@@ -4,4 +4,13 @@ class GamesControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "render_new" do
+  	user = FactoryGirl.create(:user)
+  	sign_in user
+  	get :new
+  	assert_response :success
+  end
+
+  test "create_new_game" do
+  end
 end
