e63087edf6571cf2783f0caf63a0119dee363cbd
wengs <wengcheong.sin@gmail.com>
2015-04-17 14:36:17 -0700

    save code before switch branch

--- app/models/king.rb
+++ app/models/king.rb
@@ -7,31 +7,31 @@ class King < Piece
 	end
 
 	private 
-		def valid_vertical_move?(new_x, new_y)
-			y_within_one(new_y) if same_x?(new_x)
-		end
+	def valid_vertical_move?(new_x, new_y)
+		y_within_one(new_y) if same_x?(new_x)
+	end
 
-		def valid_horizontal_move?(new_x, new_y)
-			x_within_one(new_x) if same_y?(new_y)
-		end
+	def valid_horizontal_move?(new_x, new_y)
+		x_within_one(new_x) if same_y?(new_y)
+	end
 
-		def valid_diagonal_move?(new_x, new_y)
-			x_within_one(new_x) && y_within_one(new_y) 
-		end	
+	def valid_diagonal_move?(new_x, new_y)
+		x_within_one(new_x) && y_within_one(new_y) 
+	end	
 
-		def same_x?(new_x)
-			new_x == x_coord
-		end
+	def same_x?(new_x)
+		new_x == x_coord
+	end
 
-		def same_y?(new_y)
-			new_y == y_coord
-		end
+	def same_y?(new_y)
+		new_y == y_coord
+	end
 
-		def y_within_one?(new_y)
-			(new_y - y_coord).abs == 1 
-		end
+	def y_within_one?(new_y)
+		(new_y - y_coord).abs == 1 
+	end
 
-		def x_within_one?(new_x)
-			(new_x - x_coord).abs == 1 
-		end			
+	def x_within_one?(new_x)
+		(new_x - x_coord).abs == 1 
+	end			
 end
