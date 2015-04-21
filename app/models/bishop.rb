7964c2471d511694e6dd1612a200de50040fab7a
Audrey Wong <audreydotwong@gmail.com>
2015-04-21 20:21:47 +0000

    queen bishop rook valid moves

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -1,2 +1,18 @@
 class Bishop < Piece 
+  def move_valid?(new_x, new_y)
+    valid_diagonal_move?(new_x, new_y)
+  end
+ 
+  private
+  def valid_diagonal_move?(new_x, new_y)
+    x_distance(new_x) == y_distance(new_y) && x_distance(new_x) != 0
+  end
+ 
+  def x_distance(new_x)
+    (new_x - x_coord).abs
+  end
+ 
+  def y_distance(new_y)
+    (new_y - y_coord).abs
+  end
 end
--- app/models/queen.rb
+++ app/models/queen.rb
@@ -1,2 +1,28 @@
-class Queen < Piece 
+class Queen < Piece
+  def move_valid?(new_x, new_y)
+    valid_vertical_move?(new_x, new_y)   || 
+    valid_horizontal_move?(new_x, new_y) ||
+    valid_diagonal_move?(new_x, new_y)  
+  end
+
+  private
+  def valid_vertical_move?(new_x, new_y)
+    new_x == x_coord && new_y != y_coord
+  end
+ 
+  def valid_horizontal_move?(new_x, new_y)
+    new_y == y_coord && new_x != x_coord
+  end
+
+  def valid_diagonal_move?(new_x, new_y)
+    x_distance(new_x) == y_distance(new_y) && x_distance(new_x) != 0
+  end
+ 
+  def x_distance(new_x)
+    (new_x - x_coord).abs
+  end
+ 
+  def y_distance(new_y)
+    (new_y - y_coord).abs
+  end
 end
