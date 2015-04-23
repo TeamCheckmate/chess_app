2ca6b5cfe306ff71a0dfab60f5d997dd5839ff59
Audrey Wong <audreydotwong@gmail.com>
2015-04-23 16:00:19 +0000

    another attempt to fix heroku error

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {:omniauth_callbacks => "users/omniauth_callbacks"}
+  devise_for :users, :controllers => {:omniauth_callbacks => "callbacks"}
   get "welcome/index" => 'welcome#index'
   get "welcome/about" => 'welcome#about'
   root to: 'welcome#index'
