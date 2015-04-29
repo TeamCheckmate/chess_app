8ebe7ec9a4dd10496e43704e5f55a9dbdf4a3c23
Tate Price <priceat@gmail.com>
2015-04-28 23:08:10 -0500

    Added Pawn Capture

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -2,14 +2,14 @@ class Pawn < Piece
   def move_valid?(new_x, new_y)
     return false if self.game.is_obstructed?([x_coord, y_coord], [new_x,new_y])
 
-    valid_vertical_move?(new_y)   || 
+    valid_vertical_move?(new_y, new_x)   || 
     valid_start_move?(new_y, new_x) ||
     valid_diagonal_move?(new_x, new_y)  
   end
 
   private 
-  def valid_vertical_move?(new_y)
-    y_within_one?(new_y)
+  def valid_vertical_move?(new_y, new_x)
+    y_within_one?(new_y) && same_x?(new_x)
   end
 
   def valid_start_move?(new_y, new_x)
@@ -20,7 +20,13 @@ class Pawn < Piece
   end
 
   def valid_diagonal_move?(new_x, new_y)
-    x_within_one?(new_x) && y_within_one?(new_y) 
+    if x_within_one?(new_x) && y_within_one?(new_y)
+      destn_piece = self.game.square_occupied(new_x, new_y).first
+      if !destn_piece.nil? && destn_piece.color != self.color 
+        return true
+      end
+    end
+    return false
   end 
 
   def same_x?(new_x)
